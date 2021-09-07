c1223b310afd352229ab50008262c573053c75ce
Fabiano Scorsatto <fabianoscorsatto@gmail.com>
2021-09-07 05:45:52 -0400

    Aula 3

--- src/main/java/com/digital/crud/saladereuniao/saladereuniao/model/Room.java
+++ src/main/java/com/digital/crud/saladereuniao/saladereuniao/model/Room.java
@@ -1,4 +1,27 @@
 package com.digital.crud.saladereuniao.saladereuniao.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="meetingroom")
+@Data
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "date", nullable = false)
+    private String date;
+
+    @Column(name = "startHour", nullable = false)
+    private String startHour;
+
+    @Column(name = "endHour", nullable = false)
+    private String endHour;
 }
